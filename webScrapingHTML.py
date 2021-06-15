f2f128259130dc503384b51fe6feaf86d46d46bb
Aniket Kodre <this.aniket69@gmail.com>
2021-06-15 11:17:48 -0700

    Update webScrapingHTML.py

--- webScrapingHTML.py
+++ webScrapingHTML.py
@@ -3,37 +3,21 @@ import requests
 
 url = 'https://en.wikipedia.org/'
 response = requests.get(url)
-
 page = response.content
-
-
-
-
 
 # 2) Work with page data
 from bs4 import BeautifulSoup
-
 soup = BeautifulSoup(page, 'html.parser')
-
 inTheNews = soup.find('div', id='mp-itn')
 
 # 2.1) Print news text and links
 news = inTheNews.find_all('li')
-
 # for n in news:
 #     print(n.get_text())
 #     links = n.find_all('a')
 #     for link in links:
 #         print(link['href'])
 #     print()
-
-
-
-
-
-
-
-
 
 # 2.2) Save a picture
 picDiv = inTheNews.find('div', class_='itn-img')
